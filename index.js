e9f3d98fe8bb515bfc0cbbd31dff7cfd49a9a988
nileshvijayshinde <nileshvijayshinde@gmail.com>
2023-04-27 18:32:32 +0530

    sdh

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-//importing node framework
-var express = require(‘express’);
-var app = express();//Respond with "hello world" for requests that hit our root "/"
-app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
-});//listen to port 80 by default
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+res.send('hello world');
+});
 app.listen(process.env.PORT || 80);
 module.exports = app;
-
-
-
-
-
-
-
