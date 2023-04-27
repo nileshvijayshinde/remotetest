53b6d13648f7b8660498578d0d4439a8acfedd69
nileshvijayshinde <nileshvijayshinde@gmail.com>
2023-04-27 17:54:07 +0530

    fhi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //importing node framework
 var express = require(‘express’);
-var app = express();//Respond with "hello world" for requests that hit our root "/"
+var app = express();//Respond with "hello nilesh" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
  res.send(‘hello world’);
 });//listen to port 80 by default
